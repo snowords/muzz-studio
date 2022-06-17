1ba0e493e8ebbcaec7de05f8e7a33ef21cec9303
Snoword <1430750200@qq.com>
2022-06-17 17:28:30 +0800

    build: change github url

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -20,15 +20,11 @@ const toggleLocales = () => {
       <div i="carbon-sun dark:carbon-moon" />
     </button>
 
-    <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales">
-      <div i-carbon-language />
-    </a>
-
     <RouterLink class="icon-btn mx-2" to="/about" :title="t('button.about')">
       <div i-carbon-dicom-overlay />
     </RouterLink>
 
-    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
+    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/snowords/muzz-studio" target="_blank" title="GitHub">
       <div i-carbon-logo-github />
     </a>
   </nav>
